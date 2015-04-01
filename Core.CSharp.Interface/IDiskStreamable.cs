21ba81a2da3c6b46400e3ebd36046677a4c22328
Onyeka Obi <v-oniobi@microsoft.com>
2015-04-01 09:23:27 -0700

    Added interface classes

---
+++ Core.CSharp.Interface/IDiskStreamable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation;
+
+namespace Core.CSharp.Interface
+{
+	public interface IDiskStreamable
+	{
+		void SubscribeToMemoryWatcher(IMemoryWatchable memoryWatcher);
+		IAsyncAction WriteAsync(string identifier, IEnumerable<byte> content);
+		IAsyncOperation<IEnumerable<byte>> ReadAsync(string identifier);
+		IAsyncAction DeleteAsync(string identifier);
+		IAsyncAction ReplaceAsync(string oldIdentifier, string newIdentifier, IEnumerable<byte> content);
+	}
+}
---
+++ Core.CSharp.Interface/IMemoryWatchable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Core.CSharp.Interface
+{
+    public interface IMemoryWatchable
+    {
+	    event EventHandler<object> ApplicationMemoryCriticalLimitReached;
+    }
+}
