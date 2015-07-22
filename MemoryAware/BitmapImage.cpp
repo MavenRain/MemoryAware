027f4450f8cdf30e1583a84811b64849e183b12b
Onyeka Obi <v-oniobi@microsoft.com>
2015-07-22 12:38:41 -0700

    CreateHandle method added

--- MemoryAware/BitmapImage.cpp
+++ MemoryAware/BitmapImage.cpp
@@ -3,19 +3,65 @@
 #include <BitmapImage.h>
 #include <stdio.h>
 #include <StorageMode.h>
+#include <objbase.h>
 
-using namespace std;
+#pragma comment(lib,"ole32.lib")
+
+using namespace Concurrency;
 using namespace Platform;
 using namespace Platform::Collections;
+using namespace std;
 using namespace Windows::Foundation;
 using namespace Windows::Storage;
+using namespace Windows::Web::Http;
+
 
 MemoryAware::BitmapImage::BitmapImage()
 {
 	inMemoryImages = ref new Map<String^, Windows::UI::Xaml::Media::Imaging::BitmapImage^>();
 }
 
-bool MemoryAware::BitmapImage::Create(StorageMode storageMode, String^* handle)
+bool MemoryAware::BitmapImage::CreateHandle(String^& handle)
+{
+	GUID nativeGuid;
+	if (S_OK != CoCreateGuid(&nativeGuid)) return false;
+	Guid uwpGuid(nativeGuid);
+	handle = uwpGuid.ToString() + ".handle";
+	return true;
+}
+
+bool MemoryAware::BitmapImage::Create(StorageMode storageMode, String^& handle)
+{
+	if (!CreateHandle(handle)) return false;
+	switch (storageMode)
+	{
+	case StorageMode::Memory:
+		inMemoryImages->Insert(handle, ref new Windows::UI::Xaml::Media::Imaging::BitmapImage());
+		break;
+	case StorageMode::Disk:
+		ApplicationData::Current->TemporaryFolder->CreateFileAsync(handle);
+		break;
+	}
+	return true;
+}
+
+bool MemoryAware::BitmapImage::Create(StorageMode storageMode, Windows::Foundation::Uri^ uri, Platform::String^& handle)
+{
+	if (!CreateHandle(handle)) return false;
+	switch (storageMode)
+	{
+	case StorageMode::Memory:
+		inMemoryImages->Insert(handle, ref new Windows::UI::Xaml::Media::Imaging::BitmapImage(uri));
+		break;
+	case StorageMode::Disk:
+		ApplicationData::Current->TemporaryFolder->CreateFileAsync(handle);
+		auto httpResourceResponse = 
+		break;
+	}
+	return true;
+}
+
+bool MemoryAware::BitmapImage::SetSource(StorageMode storageMode, Platform::String^, Windows::Foundation::Uri^)
 {
 	switch (storageMode)
 	{
@@ -24,4 +70,17 @@ bool MemoryAware::BitmapImage::Create(StorageMode storageMode, String^* handle)
 	case StorageMode::Disk:
 		break;
 	}
+	return true;
+}
+
+IAsyncOperation<bool>^ MemoryAware::BitmapImage::SetSourceAsync(StorageMode storageMode, Platform::String^, Windows::Foundation::Uri^)
+{
+	switch (storageMode)
+	{
+	case StorageMode::Memory:
+		break;
+	case StorageMode::Disk:
+		break;
+	}
+	return create_async([] {return true; });
 }
--- MemoryAware/BitmapImage.h
+++ MemoryAware/BitmapImage.h
@@ -1,20 +1,17 @@
 #pragma once
-
-namespace MemoryAware
-{
-	enum class StorageMode;
-}
+#include <StorageMode.h>
 
 namespace MemoryAware
 {
 	static public ref class BitmapImage sealed
 	{
-		static Platform::Collections::Map<String^, Windows::UI::Xaml::Media::Imaging::BitmapImage^>^ inMemoryImages;
+		static property Windows::Foundation::Collections::IMap<Platform::String^, Windows::UI::Xaml::Media::Imaging::BitmapImage^>^ inMemoryImages;
+		static bool CreateHandle(Platform::String^&);
 	public:
 		BitmapImage();
-		static bool Create(StorageMode, Platform::String^*);
-		static bool Create(StorageMode, Windows::Foundation::Uri^, Platform::String^*);
-		static bool SetSource(Platform::String^, Windows::Foundation::Uri^);
-		static Windows::Foundation::IAsyncOperation<bool>^ SetSourceAsync(Platform::String^, Windows::Foundation::Uri^);
+		static bool Create(StorageMode, Platform::String^&);
+		static bool Create(StorageMode, Windows::Foundation::Uri^, Platform::String^&);
+		static bool SetSource(StorageMode, Platform::String^, Windows::Foundation::Uri^);
+		static Windows::Foundation::IAsyncOperation<bool>^ SetSourceAsync(StorageMode, Platform::String^, Windows::Foundation::Uri^);
 	};
 }
