2e542d064ca3424749c88c037e3f619c21a6b405
Onyeka Obi <v-oniobi@microsoft.com>
2015-07-22 22:06:30 -0700

    First concept of storage-option BitmapImage developed

--- MemoryAware/BitmapImage.cpp
+++ MemoryAware/BitmapImage.cpp
@@ -13,6 +13,7 @@ using namespace Platform::Collections;
 using namespace std;
 using namespace Windows::Foundation;
 using namespace Windows::Storage;
+using namespace Windows::Storage::Streams;
 using namespace Windows::Web::Http;
 
 
@@ -21,66 +22,108 @@ MemoryAware::BitmapImage::BitmapImage()
 	inMemoryImages = ref new Map<String^, Windows::UI::Xaml::Media::Imaging::BitmapImage^>();
 }
 
-bool MemoryAware::BitmapImage::CreateHandle(String^& handle)
+bool MemoryAware::BitmapImage::CreateHandle(String^* handle)
 {
 	GUID nativeGuid;
 	if (S_OK != CoCreateGuid(&nativeGuid)) return false;
 	Guid uwpGuid(nativeGuid);
-	handle = uwpGuid.ToString() + ".handle";
+	*handle = uwpGuid.ToString() + ".handle";
 	return true;
 }
 
-bool MemoryAware::BitmapImage::Create(StorageMode storageMode, String^& handle)
+bool MemoryAware::BitmapImage::Create(StorageMode storageMode, String^* handle)
 {
 	if (!CreateHandle(handle)) return false;
 	switch (storageMode)
 	{
 	case StorageMode::Memory:
-		inMemoryImages->Insert(handle, ref new Windows::UI::Xaml::Media::Imaging::BitmapImage());
+		inMemoryImages->Insert(*handle, ref new Windows::UI::Xaml::Media::Imaging::BitmapImage());
 		break;
 	case StorageMode::Disk:
-		ApplicationData::Current->TemporaryFolder->CreateFileAsync(handle);
+		ApplicationData::Current->TemporaryFolder->CreateFileAsync(*handle);
 		break;
 	}
 	return true;
 }
 
-bool MemoryAware::BitmapImage::Create(StorageMode storageMode, Windows::Foundation::Uri^ uri, Platform::String^& handle)
+bool MemoryAware::BitmapImage::Create(StorageMode storageMode, Windows::Foundation::Uri^ uri, Platform::String^* handle)
 {
 	if (!CreateHandle(handle)) return false;
 	switch (storageMode)
 	{
 	case StorageMode::Memory:
-		inMemoryImages->Insert(handle, ref new Windows::UI::Xaml::Media::Imaging::BitmapImage(uri));
+		inMemoryImages->Insert(*handle, ref new Windows::UI::Xaml::Media::Imaging::BitmapImage(uri));
 		break;
 	case StorageMode::Disk:
-		ApplicationData::Current->TemporaryFolder->CreateFileAsync(handle);
-		auto httpResourceResponse = 
+		create_task(ApplicationData::Current->TemporaryFolder->CreateFileAsync(*handle)).then([handle, uri](IStorageFile^ storageFile) 
+		{
+			return create_task(storageFile->OpenAsync(FileAccessMode::ReadWrite));
+		}).then([uri](IRandomAccessStream^ randomAccessStream) 
+		{
+			return create_task((ref new HttpClient())->GetAsync(uri)).then([&randomAccessStream](HttpResponseMessage^ response) 
+			{
+				return create_task(response->Content->WriteToStreamAsync(randomAccessStream));
+			});
+		});
 		break;
 	}
 	return true;
 }
 
-bool MemoryAware::BitmapImage::SetSource(StorageMode storageMode, Platform::String^, Windows::Foundation::Uri^)
+bool MemoryAware::BitmapImage::SetSource(StorageMode storageMode, Platform::String^ handle, Windows::Foundation::Uri^ uri)
 {
 	switch (storageMode)
 	{
 	case StorageMode::Memory:
+		inMemoryImages->Lookup(handle)->UriSource = uri;
 		break;
 	case StorageMode::Disk:
+		create_task(ApplicationData::Current->TemporaryFolder->CreateFileAsync(handle)).then([handle, uri](IStorageFile^ storageFile) 
+		{
+			return create_task(storageFile->OpenAsync(FileAccessMode::ReadWrite));
+		}).then([uri](IRandomAccessStream^ randomAccessStream) 
+		{
+			return create_task((ref new HttpClient())->GetAsync(uri)).then([&randomAccessStream](HttpResponseMessage^ response) 
+			{
+				return create_task(response->Content->WriteToStreamAsync(randomAccessStream));
+			});
+		});
 		break;
 	}
 	return true;
 }
 
-IAsyncOperation<bool>^ MemoryAware::BitmapImage::SetSourceAsync(StorageMode storageMode, Platform::String^, Windows::Foundation::Uri^)
+IAsyncOperation<bool>^ MemoryAware::BitmapImage::SetSourceAsync(StorageMode storageMode, Platform::String^ handle, Windows::Foundation::Uri^ uri)
 {
 	switch (storageMode)
 	{
 	case StorageMode::Memory:
+		return create_async([handle, uri] 
+		{
+			inMemoryImages->Lookup(handle)->UriSource = uri;
+			return true;
+		});
 		break;
 	case StorageMode::Disk:
+		return create_async([handle, uri] 
+		{
+			create_task(ApplicationData::Current->TemporaryFolder->CreateFileAsync(handle)).then([handle, uri](IStorageFile^ storageFile)
+			{
+				return create_task(storageFile->OpenAsync(FileAccessMode::ReadWrite));
+			}).then([uri](IRandomAccessStream^ randomAccessStream)
+			{
+				return create_task((ref new HttpClient())->GetAsync(uri)).then([&randomAccessStream](HttpResponseMessage^ response)
+				{
+					return create_task(response->Content->WriteToStreamAsync(randomAccessStream)).then([](unsigned long long bytesRecorded)
+					{
+						return bytesRecorded > 0 ? true : false;
+					});
+				});
+			});
+			return true;
+		});
+		
 		break;
 	}
-	return create_async([] {return true; });
+	return create_async([] {return false; });
 }
--- MemoryAware/BitmapImage.h
+++ MemoryAware/BitmapImage.h
@@ -6,11 +6,11 @@ namespace MemoryAware
 	static public ref class BitmapImage sealed
 	{
 		static property Windows::Foundation::Collections::IMap<Platform::String^, Windows::UI::Xaml::Media::Imaging::BitmapImage^>^ inMemoryImages;
-		static bool CreateHandle(Platform::String^&);
+		static bool CreateHandle(Platform::String^*);
 	public:
 		BitmapImage();
-		static bool Create(StorageMode, Platform::String^&);
-		static bool Create(StorageMode, Windows::Foundation::Uri^, Platform::String^&);
+		static bool Create(StorageMode, Platform::String^*);
+		static bool Create(StorageMode, Windows::Foundation::Uri^, Platform::String^*);
 		static bool SetSource(StorageMode, Platform::String^, Windows::Foundation::Uri^);
 		static Windows::Foundation::IAsyncOperation<bool>^ SetSourceAsync(StorageMode, Platform::String^, Windows::Foundation::Uri^);
 	};
